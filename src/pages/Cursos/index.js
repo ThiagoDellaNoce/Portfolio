5d7bcda01c97809e0a26f2c8d273704215dc53a9
Thiago Della Noce <thiagodellanoce22@gmail.com>
2020-04-17 03:56:38 -0300

    melhoria na tela sobre

--- src/pages/Cursos/index.js
+++ src/pages/Cursos/index.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from "../../services/api";
 
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -8,44 +10,53 @@ import "./styles.css";
 import CursosImg from "./../../images/_certificados.png";
 
 export default function Cursos() {
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    api.get("courses", {}).then((Response) => {
+      setCourses(Response.data);
+    });
+  }, []);
+
   return (
     <div>
       <Row className="row">
-        <Col className="content" md={1}>
-        </Col>
+        <Col className="content" md={1}></Col>
         <Col md={6}>
           <h1>Cursos e certificações</h1>
         </Col>
       </Row>
-      
+
       <Row>
         <Col md={1}></Col>
         <Col md={3}></Col>
         <Col md={8}>
-          <div>
-            <Row>
-              <Col md={6}>
-                <h4>Título do Curso</h4>
-              </Col>
-              <Col md={6} style={{ float: "left" }}>
-                <h5>Plataforma</h5>
-              </Col>
-            </Row>
-            <Row>
-              <Col md={12}>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                  Cras molestie sed lorem eu volutpat. Donec id risus nec nisl 
-                  accumsan hendrerit. Nam vel ipsum quis lorem auctor congue id eu risus.
-                </p>
-              </Col>
-            </Row>
-          </div>
+          {courses.map((course) => (
+            <div key={course.id}>
+              <Row>
+                <Col md={6}>
+                  <h4>{course.title}</h4>
+                </Col>
+                <Col md={6} style={{ float: "left" }}>
+                  <h5>{course.platform}</h5>
+                </Col>
+              </Row>
+              <Row>
+                <Col md={12}>
+                  <p>{course.description}</p>
+                </Col>
+              </Row>
+            </div>
+          ))}
         </Col>
       </Row>
-          
-      <img className="img" style={{ width: "300px" }} src={ CursosImg } alt="imagem ilustrativa para cursos e certificações"/>
 
+      <img
+        className="img"
+        style={{ width: "300px" }}
+        src={CursosImg}
+        alt="imagem ilustrativa para cursos e certificações"
+      />
     </div>
-  )
+  );
 }
